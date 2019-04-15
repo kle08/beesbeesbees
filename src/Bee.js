5834fb2c9490c92655e17818ab5945c618f2459c
Kevin Le <kevinle@Kevins-MacBook-Pro-2.local>
2019-04-15 12:29:54 -0700

    Bee.js complete

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,10 @@
-var Bee = function() {
+var Bee = function(food, eat) {
+    Grub.call(this, food, eat);
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'keep on growing';
+    
+    
 };
-
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
