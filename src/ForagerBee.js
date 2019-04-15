c9f42e4581ef8fe449df6b78a567dbe1113ab89a
Kevin Le <kevinle@Kevins-MacBook-Pro-2.local>
2019-04-15 12:58:24 -0700

    honeyMaker and ForagerBee complete

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
-var ForagerBee = function() {
+var ForagerBee = function(color, food, eat) {
+    Bee.call(this, color, food, eat);
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(treasure) {
+    this.treasureChest.push(treasure);
+  };
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
-var HoneyMakerBee = function() {
+var HoneyMakerBee = function(color, food, eat) {
+    Bee.call(this, color, food, eat);
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
 };
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+    this.honeyPot++;
+};
+HoneyMakerBee.prototype.giveHoney = function() {
+    this.honeyPot--;
+};
+
+
 
